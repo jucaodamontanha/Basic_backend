28032d12b58378c70613703cbee7bbcbd818c841
lineker xavier <jucaodamontanha@gmail.com>
2025-03-28 01:20:19 -0300

    criado classe de email e adicionado a logo no pdf

--- src/main/java/br/com/basic/basic/ordemDeServico/PdfService.java
+++ src/main/java/br/com/basic/basic/ordemDeServico/PdfService.java
@@ -5,19 +5,16 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.stereotype.Service;
-
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
-import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Base64;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.*;
 
 @Service
 public class PdfService {
@@ -28,24 +25,60 @@ public class PdfService {
         PdfWriter.getInstance(document, out);
 
         document.open();
-        document.add(new Paragraph("Ordem de Serviço"));
-        document.add(new Paragraph("Nome do Cliente: " + ordemDeServicoModel.getNomeCliente()));
-        document.add(new Paragraph("Endereço: " + ordemDeServicoModel.getEndereco()));
-        document.add(new Paragraph("Número: " + ordemDeServicoModel.getNumero()));
-        document.add(new Paragraph("Cidade: " + ordemDeServicoModel.getCidade()));
-        document.add(new Paragraph("Número do Contrato: " + ordemDeServicoModel.getNumeroContrato()));
-        document.add(new Paragraph("Email do Cliente: " + ordemDeServicoModel.getEmailCliente()));
-        document.add(new Paragraph("Nome do Atendente: " + ordemDeServicoModel.getNomeAtendente()));
-        document.add(new Paragraph("Supervisor: " + ordemDeServicoModel.getSupervisor()));
-        document.add(new Paragraph("Tipo de Atendimento: " + ordemDeServicoModel.getTipoAtendimento()));
-        document.add(new Paragraph("Observação: " + ordemDeServicoModel.getObservacao()));
+
+        // Adiciona a logo
+        String logoPath = "C:\\Users\\jucao\\Desktop\\Basic_backend\\src\\main\\resources\\assets\\logoBasic.jpg"; // Substitua pelo caminho da sua logo
+        Image logo = Image.getInstance(logoPath);
+        logo.setAlignment(Element.ALIGN_CENTER);
+        logo.scaleToFit(100, 100); // Ajuste o tamanho conforme necessário
+        document.add(logo);
+
+        // Estilização de fontes
+        Font titleFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 16, BaseColor.BLACK);
+        Font normalFont = FontFactory.getFont(FontFactory.HELVETICA, 12, BaseColor.BLACK);
+
+        // Título centralizado
+        Paragraph title = new Paragraph("Ordem de Serviço", titleFont);
+        title.setAlignment(Element.ALIGN_CENTER);
+        title.setSpacingAfter(10);
+        document.add(title);
+
+        // Adiciona os detalhes do cliente em uma tabela
+        PdfPTable table = new PdfPTable(2); // 2 colunas
+        table.setWidthPercentage(100);
+        table.setSpacingBefore(10f);
+        table.setSpacingAfter(10f);
+
+        table.addCell(new PdfPCell(new Paragraph("Nome do Cliente:", titleFont)));
+        table.addCell(new PdfPCell(new Paragraph(ordemDeServicoModel.getNomeCliente(), normalFont)));
+        table.addCell(new PdfPCell(new Paragraph("Endereço:", titleFont)));
+        table.addCell(new PdfPCell(new Paragraph(ordemDeServicoModel.getEndereco(), normalFont)));
+        table.addCell(new PdfPCell(new Paragraph("Número:", titleFont)));
+        table.addCell(new PdfPCell(new Paragraph(ordemDeServicoModel.getNumero(), normalFont)));
+        table.addCell(new PdfPCell(new Paragraph("Cidade:", titleFont)));
+        table.addCell(new PdfPCell(new Paragraph(ordemDeServicoModel.getCidade(), normalFont)));
+        table.addCell(new PdfPCell(new Paragraph("Número do Contrato:", titleFont)));
+        table.addCell(new PdfPCell(new Paragraph(ordemDeServicoModel.getNumeroContrato(), normalFont)));
+        table.addCell(new PdfPCell(new Paragraph("Email do Cliente:", titleFont)));
+        table.addCell(new PdfPCell(new Paragraph(ordemDeServicoModel.getEmailCliente(), normalFont)));
+        table.addCell(new PdfPCell(new Paragraph("Nome do Atendente:", titleFont)));
+        table.addCell(new PdfPCell(new Paragraph(ordemDeServicoModel.getNomeAtendente(), normalFont)));
+        table.addCell(new PdfPCell(new Paragraph("Supervisor:", titleFont)));
+        table.addCell(new PdfPCell(new Paragraph(ordemDeServicoModel.getSupervisor(), normalFont)));
+        table.addCell(new PdfPCell(new Paragraph("Tipo de Atendimento:", titleFont)));
+        table.addCell(new PdfPCell(new Paragraph(ordemDeServicoModel.getTipoAtendimento(), normalFont)));
+        table.addCell(new PdfPCell(new Paragraph("Observação:", titleFont)));
+        table.addCell(new PdfPCell(new Paragraph(ordemDeServicoModel.getObservacao(), normalFont)));
+
+        document.add(table);
 
         // Adiciona a imagem da assinatura
         if (ordemDeServicoModel.getAssinatura() != null && !ordemDeServicoModel.getAssinatura().isEmpty()) {
             String assinaturaBase64 = ordemDeServicoModel.getAssinatura().replaceFirst("^data:image/[^;]+;base64,", "");
             byte[] assinaturaBytes = Base64.getDecoder().decode(assinaturaBase64);
             Image assinatura = Image.getInstance(assinaturaBytes);
-            document.add(new Paragraph("Assinatura:"));
+            assinatura.setAlignment(Element.ALIGN_CENTER);
+            document.add(new Paragraph("Assinatura:", titleFont));
             document.add(assinatura);
         }
 
@@ -56,12 +89,14 @@ public class PdfService {
                     String fotoBase64 = foto.replaceFirst("^data:image/[^;]+;base64,", "");
                     byte[] fotoBytes = Base64.getDecoder().decode(fotoBase64);
                     Image image = Image.getInstance(fotoBytes);
-                    document.add(new Paragraph("Foto:"));
+                    image.setAlignment(Element.ALIGN_CENTER);
+                    document.add(new Paragraph("Foto:", titleFont));
                     document.add(image);
                 } else if (foto.startsWith("file:///")) {
                     byte[] fotoBytes = Files.readAllBytes(Paths.get(foto.replaceFirst("file:///", "")));
                     Image image = Image.getInstance(fotoBytes);
-                    document.add(new Paragraph("Foto:"));
+                    image.setAlignment(Element.ALIGN_CENTER);
+                    document.add(new Paragraph("Foto:", titleFont));
                     document.add(image);
                 }
             }
